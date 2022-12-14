76402bccbc9045c43f6879262e21527f9775fc06
Airdeon <airdeon@gmail.com>
2022-12-14 17:26:09 +0100

    debut des test

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from lettings.models import Address, Letting
+import pytest
+
 
 # Create your tests here.
+@pytest.mark.django_db
+def test_letting_index_page_code_status_and_title():
+    client = Client()
+    response = client.get(reverse("lettings_index"))
+    assert response.status_code == 200
+    assert "<title>Lettings</title>" in response.content.decode()
+
+
+@pytest.mark.django_db
+def test_letting_page_code_status_and_title():
+    client = Client()
+    address = Address.objects.create(
+        number=8, street="street_test", city="city_test", state="state_test", zip_code=12345, country_iso_code="033"
+    )
+    Letting.objects.create(title="test_title", address=address)
+    response = client.get(reverse("letting", kwargs={"letting_id": 1}))
+
+    assert response.status_code == 200
+    assert "<title>Lettings</title>" in response.content.decode()
