7d628a3d7c77df53dd74b6a286b9923828b94179
Airdeon <airdeon@gmail.com>
2023-01-12 14:50:36 +0100

    test config

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -123,7 +123,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = "/static/"
 
 sentry_sdk.init(
-    dsn=config("SENTRY_KEY"),
+    dsn=os.environ.get('SENTRY_KEY'),
     integrations=[
         DjangoIntegration(),
     ],
